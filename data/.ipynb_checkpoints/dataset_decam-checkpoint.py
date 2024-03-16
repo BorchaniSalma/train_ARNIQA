42e94c0b1119f7ceb036b149574fa553ba11e424
BorchaniSalma <sborchan@uwyo.edu>
2024-03-15 18:52:00 -0700

    random crop

--- data/.ipynb_checkpoints/dataset_decam-checkpoint.py
+++ data/.ipynb_checkpoints/dataset_decam-checkpoint.py
@@ -9,8 +9,8 @@ import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 import os
-from utils.utils_data import distort_decam_images, resize_crop
-from utils.utils import PROJECT_ROOT
+from torchvision import transforms
+from utils.utils_data import distort_decam_images, get_distortion_names,get_decam_distortions_composition,li_distort,li_distort_names
 
 class DECAMDataset(Dataset):
     """
@@ -36,33 +36,21 @@ class DECAMDataset(Dataset):
     def __init__(self,
                  root: str,
                  patch_size: int = 224,
-                 max_distortions: int = 4,
+                 max_distortions: int = 2,
                  num_levels: int = 5,
                  pristine_prob: float = 0.05):
 
-        root = Path(root)
-
-        
-        filenames_csv_path = "../data/decam_dr10_good_exp.csv"
-        exp_df = pd.read_csv(filenames_csv_path, header=None, names=["expnum"])
-
-        # Path to FITS file containing image data
-        file_path = "/global/cfs/cdirs/cosmo/work/legacysurvey/dr10/survey-ccds-decam-dr10.fits.gz"
-        image_table = Table.read(file_path)
-
-        # List to store paths of selected images
-        self.ref_images = []
-        self.hdu_numbers = []
-
-        test = image_table[:400]
-        idx = np.isin(test["expnum"], exp_df["expnum"])
-        matched_exp = test[idx]
-        self.ref_images =  matched_exp["image_filename"]
-        self.hdu_numbers = matched_exp['image_hdu']
-
-
         # Convert paths to Path objects
-        self.ref_images = [Path(root,path) for path in self.ref_images]
+        ref_images_csv_path = "/global/homes/s/salmab/ARNIQA/data/ref_images.csv"
+        ref_images = pd.read_csv(ref_images_csv_path)
+        hdu_numbers_csv_path = "/global/homes/s/salmab/ARNIQA/data/hdu_numbers.csv"
+        hdu_numbers = pd.read_csv(hdu_numbers_csv_path)
+     
+        ref_images = ref_images['ref_images'].tolist()
+        hdu_numbers = hdu_numbers['hdu_numbers'].tolist()
+
+        self.ref_images = [Path(path) for path in ref_images]
+        self.hdu_numbers = [hdu for hdu in hdu_numbers]
 
         self.patch_size = patch_size
         self.max_distortions = max_distortions
@@ -78,18 +66,25 @@ class DECAMDataset(Dataset):
         hdu_number = self.hdu_numbers[index]
         hdul_A = fits.open(img_A_path)
         img_A = hdul_A[hdu_number].data
-
         # Select another exposure randomly
         other_exp_index = np.random.choice(np.setdiff1d(range(len(self.ref_images)), [index]))
         img_B_path = self.ref_images[other_exp_index]
         hdul_B = fits.open(img_B_path)
         img_B = hdul_B[hdu_number].data
         
-        # Resize and crop
-        img_A_orig = torch.tensor(img_A[512:1024, 512:1024]).unsqueeze(0)
-        img_B_orig = torch.tensor(img_B[512:1024, 512:1024]).unsqueeze(0)
+        # Define the RandomCrop transformation
+        random_crop = transforms.RandomCrop(1024)
+        
+        img_A_orig = torch.tensor(img_A).unsqueeze(0)
 
+        # Apply RandomCrop to the original image
+        img_A_orig = random_crop(img_A_orig)
+        
+        img_B_orig = torch.tensor(img_B).unsqueeze(0)
 
+        # Apply RandomCrop to the original image
+        img_B_orig = random_crop(img_B_orig)        
+        
         distort_functions_A = []
         distort_values_A = []
         distort_functions_B = []
@@ -101,23 +96,21 @@ class DECAMDataset(Dataset):
                                                                                  max_distortions=self.max_distortions,
                                                                                  num_levels=self.num_levels)
 
-        # Distort images with (1 - self.pristine_prob) probability for image B
-        if random.random() > self.pristine_prob and self.max_distortions > 0:
-            img_B_orig, distort_functions_B, distort_values_B = distort_decam_images(img_B_orig,
-                                                                                 max_distortions=self.max_distortions,
-                                                                                 num_levels=self.num_levels)
+        # Use the same distortions for image B
+        img_B_orig, distort_functions_B, distort_values_B = distort_decam_images(img_B_orig, distort_functions=distort_functions_A, distort_values=distort_values_A)
 
-        img_A_orig = self.normalize(img_A_orig)
-        img_B_orig = self.normalize(img_B_orig)
+
+        distort_functions_A_names = get_distortion_names(distort_functions_A, li_distort_names)
+        distort_functions_B_names = get_distortion_names(distort_functions_B, li_distort_names)
 
         # Pad to make the length of distort_functions and distort_values equal for all samples
-        distort_functions_A = [f.__name__ for f in distort_functions_A]
-        distort_functions_A += [""] * (self.max_distortions - len(distort_functions_A))
+        distort_functions_A_names += [""] * (self.max_distortions - len(distort_functions_A_names))
         distort_values_A += [torch.inf] * (self.max_distortions - len(distort_values_A))
 
-        distort_functions_B = [f.__name__ for f in distort_functions_B]
-        distort_functions_B += [""] * (self.max_distortions - len(distort_functions_B))
+        distort_functions_B_names += [""] * (self.max_distortions - len(distort_functions_B_names))
         distort_values_B += [torch.inf] * (self.max_distortions - len(distort_values_B))
+
+       
 
         return {
             "img_A_orig": img_A_orig,"img_B_orig": img_B_orig,
@@ -125,6 +118,7 @@ class DECAMDataset(Dataset):
             "distortion_functions_B": distort_functions_B, "distortion_values_B": distort_values_B
         }
 
+    
     def __len__(self) -> int:
         return len(self.ref_images)
 
